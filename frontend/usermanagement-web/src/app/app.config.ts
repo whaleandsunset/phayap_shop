45ef80d4486fd4970c700b0cdf3ee2206a742484
whaleandsunset <168166276+whaleandsunset@users.noreply.github.com>
2026-05-29 23:25:08 +0700

    chore: remove old usermanagement web

--- frontend/usermanagement-web/src/app/app.config.ts
+++
@@ -1,9 +0,0 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideHttpClient()],
-};
--- frontend/usermanagement-web/src/app/app.routes.ts
+++
@@ -1,7 +0,0 @@
-import { Routes } from '@angular/router';
-import { UserManagement } from './user-management/user-management';
-
-export const routes: Routes = [
-  { path: 'user-management', component: UserManagement },
-  { path: '**', redirectTo: '' },
-];
